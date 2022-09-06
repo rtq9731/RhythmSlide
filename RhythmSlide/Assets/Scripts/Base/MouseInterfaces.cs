14f22cca0492fff5c9580b0fcbdb3a586b2dc2ac
COCONUT <64309783+rtq9731@users.noreply.github.com>
2022-09-06 11:40:24 +0900

    오브젝트 클릭 반영을 위해서 약간의 스크립트 추가

---
+++ RhythmSlide/Assets/Scripts/Base/MouseInterfaces.cs
@@ -0,0 +1,14 @@
+public interface IGetMouseButtonHandler
+{
+    public void GetMouseButton();
+}
+
+public interface IGetMouseButtonDownHandler
+{
+    public void GetMouseButtonDown();
+}
+
+public interface IGetMouseButtonUpHandler
+{
+    public void GetMouseButtonUp();
+}
